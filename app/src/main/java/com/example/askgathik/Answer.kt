8afff72e29d312dda2bc29a05e899d8b95b41aa1
Sai Karthik <roysteamer@gmail.com>
2022-07-31 20:09:16 +0530

    some stuff

--- app/src/main/java/com/example/askgathik/Answer.kt
+++ app/src/main/java/com/example/askgathik/Answer.kt
@@ -1,18 +1,29 @@
 package com.example.askgathik
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+
 
 class Answer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_answer)
-        val answer="you mom, sheesh "
+
+        val answer="you mom, sheesh"
         val question=intent.getStringExtra("Question")
         val answerView = findViewById<TextView>(R.id.answerView)
-        answerView.text="The answer to "+question+" is, \n"+answer
+        answerView.text=getString(R.string.answer_message, question, answer)
 
-
+        val btn = findViewById<Button>(R.id.buttonAgain)
+        btn.setOnClickListener {
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/askgathik/MainActivity.kt
+++ app/src/main/java/com/example/askgathik/MainActivity.kt
@@ -3,44 +3,42 @@ package com.example.askgathik
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.animation.AnimationUtils
+import android.transition.Fade
+import android.transition.Transition
+import android.transition.TransitionManager
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
-import android.widget.Toast.makeText
 import com.google.android.material.snackbar.Snackbar
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
+    override fun onResume() {
+        super.onResume();
+        val enterQuestion = findViewById<EditText>(R.id.enterQuestion)
+        enterQuestion.text.clear()
+
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun onAsk(view: View) {
-        val question : String = findViewById<EditText>(R.id.enterQuestion).text.toString()
-        if (question.isNotBlank()){
 
-            val wholeView =
-                findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.whole)
-            val animation = AnimationUtils.loadAnimation(this, R.anim.fade_out)
-            wholeView.startAnimation(animation)
-            Executors.newSingleThreadScheduledExecutor().schedule({
-                wholeView.alpha = 0f
-            }, 500, TimeUnit.MILLISECONDS)
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener{
 
-
-            val intent = Intent(this@MainActivity, Answer::class.java)
+            val question : String = findViewById<EditText>(R.id.enterQuestion).text.toString()
+            if (question.isNotBlank()){
+                val intent = Intent(applicationContext, Answer::class.java)
             intent.putExtra("Question", question)
             startActivity(intent)
-        }
+            }
 
-        else {
-            Snackbar.make(view, "Please Enter a Valid Question", Snackbar.LENGTH_SHORT).show()
+            else {
+            Snackbar.make(findViewById(R.id.button), "Please Enter a Valid Question", Snackbar.LENGTH_SHORT).show()
 
+            }
         }
 
     }
