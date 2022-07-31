9bc2786bc18e47b69df22c5eee3bdfa9e9f57c77
Sai Karthik <roysteamer@gmail.com>
2022-08-01 00:53:58 +0530

    Animations, Finally!

--- app/src/main/java/com/example/askgathik/Answer.kt
+++ app/src/main/java/com/example/askgathik/Answer.kt
@@ -1,29 +1,65 @@
 package com.example.askgathik
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Window
+import android.os.Handler
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.transition.platform.MaterialSharedAxis
 
 
 class Answer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        window.requestFeature(Window.FEATURE_ACTIVITY_TRANSITIONS)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_answer)
 
-        val answer="you mom, sheesh"
-        val question=intent.getStringExtra("Question")
+        val title = findViewById<TextView>(R.id.textView2)
         val answerView = findViewById<TextView>(R.id.answerView)
-        answerView.text=getString(R.string.answer_message, question, answer)
-
         val btn = findViewById<Button>(R.id.buttonAgain)
+        val question : String = intent.getStringExtra("Question").toString()
+        val answer="you mom, sheesh"
+
+
+        window.enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply{
+            duration = 1000
+        }
+
+        //window.allowEnterTransitionOverlap
+
+        //window.exitTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply{
+        //    duration = 1000
+        //}
+
+        val animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+
+        title.startAnimation(animation)
+
+        val animationSlow = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+        animationSlow.startOffset = 1000
+        val animationSlower = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+        animationSlower.startOffset = 2000
+
+        answerView.startAnimation(animationSlow)
+
+        btn.startAnimation(animationSlower)
+
+         //animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.moveback);
+        //title.startAnimation(animation)
+
+            if (question.isNotBlank()){
+                answerView.text=getString(R.string.answer_message, question, answer)
+            }
+
+
+
+            else{
+                answerView.text=getString(R.string.answer_message_default)
+            }
+
         btn.setOnClickListener {
-            finish()
+            finishAfterTransition()
         }
     }
 }
--- app/src/main/java/com/example/askgathik/MainActivity.kt
+++ app/src/main/java/com/example/askgathik/MainActivity.kt
@@ -1,45 +1,72 @@
 package com.example.askgathik
 
+import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.Fade
-import android.transition.Transition
-import android.transition.TransitionManager
-import android.view.ViewGroup
+import android.os.Handler
 import android.widget.Button
 import android.widget.EditText
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.transition.platform.MaterialSharedAxis
 
 
 class MainActivity : AppCompatActivity() {
-    override fun onResume() {
+
+    /*override fun onResume() {
         super.onResume();
         val enterQuestion = findViewById<EditText>(R.id.enterQuestion)
         enterQuestion.text.clear()
-
-    }
-
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //window.enterTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply{
+        //    duration = 1000
+        //}
+
+        window.exitTransition = MaterialSharedAxis(MaterialSharedAxis.Z, true).apply {
+          duration = 1000
+        }
+
         val btn = findViewById<Button>(R.id.button)
         btn.setOnClickListener{
 
             val question : String = findViewById<EditText>(R.id.enterQuestion).text.toString()
-            if (question.isNotBlank()){
-                val intent = Intent(applicationContext, Answer::class.java)
-            intent.putExtra("Question", question)
-            startActivity(intent)
-            }
 
-            else {
-            Snackbar.make(findViewById(R.id.button), "Please Enter a Valid Question", Snackbar.LENGTH_SHORT).show()
+            val intent = Intent(applicationContext, Answer::class.java)
+            intent.putExtra ("Question", question)
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
 
-            }
+            Handler().postDelayed({
+                val enterQuestion = findViewById<EditText>(R.id.enterQuestion)
+                enterQuestion.text.clear() }, 400)
+
         }
 
+                //Snackbar.make(findViewById(R.id.button), "Please Enter a Valid Question", Snackbar.LENGTH_SHORT).show()
     }
 }
+
+
+
+/* TODO:
+    implement more bouncy animations
+    Add More People:
+        Satvik
+        Bavya
+        Bhargav?
+        Abhinay
+        Karthik
+        Ramesh Sir
+     Jokes:
+        call the person when button is pressed
+        prank with sound?
+    Icons for people
+    NOTE:
+        Animations might be a way to add a delay. Just make a animation that does nothing
+        but takes some time to complete. Needs further testing
+
+ */
